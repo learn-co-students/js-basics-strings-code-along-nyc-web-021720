5348dc28af5db7660e6b0ed089dc21e416c0b9ce
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-30 13:15:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = `Joey`;
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
