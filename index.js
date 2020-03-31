a0f2f7dcfdc60cc5ae3f45573bd10c16ab6d37a8
anoorzi <anoorzi123@gmail.com>
2020-03-31 18:02:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+let currentUser = "string"
+let welcomeMessage = "Welcome to Flatbook, "
+
+welcomeMessage =welcomeMessage + currentUser + "!"
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+
+
+excitedWelcomeMessage = excitedWelcomeMessage + "STRING" +"!"
+
+let shortGreeting = "Welcome, "
+
+shortGreeting += currentUser[0]+"!"
